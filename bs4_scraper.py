4b368da65fc8619efd59e485e2410f73f5ca106b
yothebob <b.cmoderndesigns@gmail.com>
2021-12-05 15:19:18 -0800

    finished grab_tag_variable(), tested with grabbing hrefs

--- bs4_scraper.py
+++ bs4_scraper.py
@@ -29,27 +29,46 @@ def remove_tags(tag_list):
                 contents += str(letter)
     return content_list
 
+
 def write_to_file(filename,list):
     f = open(filename,'w+')
     for item in list:
         f.write(str(item) + "\n")
-    f.close(
+    f.close()
+
 
 def grab_tag_variable(tag_list,variable):
     content_list = []
-    
+    variable += '="'
+    print(variable)
     for tag in tag_list:
-    if variable in str(tag):
-        variable_string = ""
-        variable_index = 0
-        variable_found = False
-        contents = ""
+        if variable in str(tag):
+            variable_string = ""
+            variable_index = 0
+            variable_found = False
+            contents = ""
+            for letter in str(tag):
+                if letter == variable[variable_index] and variable_found == False:
+                    variable_string += letter
+                    if variable_index < (len(variable) -1):
+                        variable_index += 1
+                    print(variable,variable_string, variable_index)
 
-        for letter in str(tag):
-            if letter == variable[variable_index]:
-                variable_string += letter
-                variable_index += 1
-            if variable_string == variable:
-                variable_found = True
-            else:
-                return
+                if variable_string == variable:
+                    variable_found = True
+
+                if variable_found == True and letter != '"':
+                    print("added to contents")
+                    contents += letter
+
+                elif variable_found == True and letter == '"':
+                    if contents:
+                        print(f"appended {contents} to content_list")
+                        content_list.append(contents)
+                        break
+                # elif letter == variable[variable_index] and variable_found == False:
+                #     print(variable,variable_string)
+                #     variable_string += letter
+                #     variable_index += 1
+                #     contents += letter
+    return content_list
